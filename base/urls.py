b7cf4b62d61507cdc8da98ab472d897bf82e1b70
abdurashidovxumoyun <abdurashidovxumoyun7@gmail.com>
2025-03-08 22:08:50 +0300

    main

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('blog_classic_1/', Blog_classic_1_right.as_view(), name='blog_1_right'),
-    path('')
+    path('blog_classic_1_right/', Blog_classic_1_right.as_view(), name='Blog_Classic_Right1'),
+    path('Blog_classic_1/',Blog_classic_1.as_view(), name='Blog_Classic_1'),
+    path('Blog_grid_2_left/', Blog_grid_2_left.as_view(), name='Blog_Grid_2'),
+    path('Blog_grid_2_columns/', Blog_grid_2_columns.as_view(), name='Blog_Grid_2_Columns'),
+    path('Blog_grid_3_columns_right/', Blog_grid_3_columns_right.as_view(), name='Blog_Grid_3_Columns_Right'),
+    path('Blog_grid_3_columns/', Blog_grid_3_columns.as_view(), name='Blog_Grid_3_Columns')
 ]
--- base/views.py
+++ base/views.py
@@ -6,3 +6,21 @@ class Home(TemplateView):
 
 class Blog_classic_1_right(TemplateView):
     template_name='pages/blog_classic_1_column_right_sidebar.html'
+
+class Blog_classic_1(TemplateView):
+    template_name='pages/blog_classic_1_column.html'
+
+class Blog_grid_2_left(TemplateView):
+    template_name='pages/blog_grid_2_columns_left_sidebar.html'
+
+class Blog_grid_2_columns(TemplateView):
+    template_name='pages/blog_grid_2_columns.html'
+
+class Blog_grid_3_columns_right(TemplateView):
+    template_name='pages/blog_grid_3_columns_right_sidebar.html'
+
+class Blog_grid_3_columns(TemplateView):
+    template_name='pages/blog_grid_3_columns.html'
+
+class Blog_grid_4(TemplateView):
+    template_name='pages/blog_grid_4_columns.html'
